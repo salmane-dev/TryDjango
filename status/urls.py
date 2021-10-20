64dc7832ebf96ab8523c344d2ac1b874de3a08ab
Salmane <salmane-ben@live.com>
2021-10-20 16:36:48 +0100

    mitarbeiten, nur spass

--- status/urls.py
+++ status/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index.as_view(), name='index'),
-
+    path('login', views.Login.as_view(), name='login'),
 ]
--- status/views.py
+++ status/views.py
@@ -1,12 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
 
  
-class index(View):
+class index(LoginRequiredMixin, View):
     template = 'index.html'
+    login_url = '/login/'
+
     def get(self, request):
         return render(request, self.template)
+
+
+class index(LoginRequiredMixin, View):
+    template = 'index.html'
+    login_url = '/login/'
+
+    def get(self, request):
+        return render(request, self.template)
+
+
+
+class Login(View):
+    template = 'login.html'
+
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, self.template, {'form': form})
+
+
+    def post(self, request):
+        form = AuthenticationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, self.template, {'form': form, 'data':"my awesome data"})
+
+
 
 
 def page2(request):
